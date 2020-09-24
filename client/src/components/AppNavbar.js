0c9453c4b8bab261fdffcbeeee33c91e4a547f31
ARIMANTAS Jakutonis <axj946@cs.bham.ac.uk>
2020-09-24 13:40:29 +0300

    Finished navbar

---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
+
+const AppNavbar = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="dark" dark expand="sm">
+        <NavbarBrand href="/">ShoppingList</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="https://github.com/arimantasjakutonis">GitHub</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default AppNavbar;
